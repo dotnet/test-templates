c583e560a8a59cd93d6c24157050709d69a791c6
Shen Chen <shech@microsoft.com>
2018-10-23 10:44:02 -0700

    Fix the nuget package problem

--- Templates/CSharp/EdgeDriverTemplate/ProjectTemplates/CSharp/Test/EdgeDriverTest.cs
+++ Templates/CSharp/EdgeDriverTemplate/ProjectTemplates/CSharp/Test/EdgeDriverTest.cs
@@ -6,29 +6,30 @@ namespace $safeprojectname$
     [TestClass]
     public class EdgeDriverTest
     {
-    // In order to use web driver of Edge, it is required to download and install the Edge Web Driver from
-    // https://developer.microsoft.com/en-us/microsoft-edge/tools/webdriver/
-    // Then add it to your PATH enviroment variable and run it at the background.
-        EdgeDriver Driver;
+        // In order to use web driver of Edge, it is required to download and install the Edge Web Driver from
+        // https://developer.microsoft.com/en-us/microsoft-edge/tools/webdriver/
+        // Then add it to your PATH enviroment variable and run it at the background.
+
+        EdgeDriver _driver;
         [TestInitialize]
         public void EdgeDriverInitialize()
         {
            // Initialize edge driver 
-            Driver = new EdgeDriver();
+            _driver = new EdgeDriver();
         }
 
         [TestMethod]
         public void VerifyPageTitle()
         {
             // Add your own test logic
-            Driver.Url = "https://www.bing.com";
+            _driver.Url = "https://www.bing.com";
             Assert.AreEqual("Bing", Driver.Title);
         }
 
         [TestCleanup]
         public void EdgeDriverCleanup()
         {
-            Driver.Quit();
+            _driver.Quit();
         }
     }
 }
