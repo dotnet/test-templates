850a67a70ff5540ffab3850c962963bf4f040880
Sarabjot Singh <sasin@microsoft.com>
2019-02-20 19:10:01 +0530

    Refactored restore toolset

--- eng/restore-toolset.ps1
+++ eng/restore-toolset.ps1
@@ -11,20 +11,21 @@ function InitializeCustomSDKToolset {
   }
 
   $cli = InitializeDotnetCli -install:$true
+  $dotnetRoot = $env:DOTNET_INSTALL_DIR
+  $installScript = GetDotNetInstallScript $dotnetRoot
+  & $installScript -InstallDir $env:DOTNET_INSTALL_DIR -NoPath -Version "3.0.100-preview-010184"
+
   InstallDotNetSharedFramework "1.0.5"
   InstallDotNetSharedFramework "1.1.2"
   InstallDotNetSharedFramework "2.1.0"
   InstallDotNetSharedFramework "2.2.1"
   InstallDotNetSharedFramework "2.2.2"
-  InstallDotNetSharedFramework "LATEST"
 }
 
 function InstallDotNetSharedFramework([string]$version) {
-  $dotnetRoot = $env:DOTNET_INSTALL_DIR
   $fxDir = Join-Path $dotnetRoot "shared\Microsoft.NETCore.App\$version"
 
-  if (!(Test-Path $fxDir)) {
-    $installScript = GetDotNetInstallScript $dotnetRoot
+  if (!(Test-Path $fxDir)) {    
     & $installScript -Version $version -InstallDir $dotnetRoot -Runtime "dotnet"
 
     if($lastExitCode -ne 0) {
