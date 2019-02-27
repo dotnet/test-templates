8349c164e1ea9b8d9c1fd33102fcdee4fad5a3f1
Mayank Bansal <maban@microsoft.com>
2019-02-27 14:58:54 +0530

    Add Default Main page to generate IXamlMetadataProvider info

---
+++ Templates/CSharp/UWP/ProjectTemplates/CSharp/Windows UAP/UWP/UnitTestAppMainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace $safeprojectname$
+{
+    public sealed partial class UnitTestAppMainPage : Page
+    {
+        public UnitTestAppMainPage()
+        {
+            this.InitializeComponent();
+        }
+
+    }
+}
