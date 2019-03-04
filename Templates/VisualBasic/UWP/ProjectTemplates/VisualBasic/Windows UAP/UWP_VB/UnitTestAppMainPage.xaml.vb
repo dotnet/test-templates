e1426da054f00208bcd12d86debcdbba0d1b5e16
mayankbansal018 <maban@microsoft.com>
2019-03-04 22:20:44 +0530

    Update UnitTestAppMainPage.xaml.vb

--- Templates/VisualBasic/UWP/ProjectTemplates/VisualBasic/Windows UAP/UWP_VB/UnitTestAppMainPage.xaml.vb
+++ Templates/VisualBasic/UWP/ProjectTemplates/VisualBasic/Windows UAP/UWP_VB/UnitTestAppMainPage.xaml.vb
@@ -1,6 +1,4 @@
-﻿' The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
-
-''' <summary>
+﻿''' <summary>
 ''' An empty page that can be used on its own or navigated to within a Frame.
 ''' </summary>
 Public NotInheritable Class UnitTestAppMainPage
