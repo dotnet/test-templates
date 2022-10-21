43728f29e650d0ed4204a2f04ed400c6edef7bdc
Jakub Jareš <me@jakubjares.com>
2022-10-21 15:38:27 +0200

    Add content

--- template_feed/Microsoft.DotNet.Test.Playwright.ProjectTemplates.8.0/content/NUnit-CSharp/UnitTest1.cs
+++ template_feed/Microsoft.DotNet.Test.Playwright.ProjectTemplates.8.0/content/NUnit-CSharp/UnitTest1.cs
@@ -1,15 +1,27 @@
 namespace Company.TestProject1;
 
-public class Tests
+[Parallelizable(ParallelScope.Self)]
+[TestFixture]
+public class Tests : PageTest
 {
-    [SetUp]
-    public void Setup()
-    {
-    }
-
     [Test]
-    public void Test1()
+    public async Task HomepageHasPlaywrightInTitleAndGetStartedLinkLinkingtoTheIntroPage()
     {
-        Assert.Pass();
+        await Page.GotoAsync("https://playwright.dev");
+
+        // Expect a title "to contain" a substring.
+        await Expect(Page).ToHaveTitleAsync(new Regex("Playwright"));
+
+        // create a locator
+        var getStarted = Page.Locator("text=Get Started");
+
+        // Expect an attribute "to be strictly equal" to the value.
+        await Expect(getStarted).ToHaveAttributeAsync("href", "/docs/intro");
+
+        // Click the get started link.
+        await getStarted.ClickAsync();
+
+        // Expects the URL to contain intro.
+        await Expect(Page).ToHaveURLAsync(new Regex(".*intro"));
     }
 }
