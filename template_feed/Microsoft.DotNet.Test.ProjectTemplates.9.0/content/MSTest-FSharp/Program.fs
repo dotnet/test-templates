1f2282ebd12d918a923cd79812c1f8ce5dd9a2b1
Amaury Levé <amauryleve@microsoft.com>
2024-01-25 11:47:09 +0100

    Enable MSTest runner

--- template_feed/Microsoft.DotNet.Test.ProjectTemplates.9.0/content/MSTest-FSharp/Program.fs
+++ template_feed/Microsoft.DotNet.Test.ProjectTemplates.9.0/content/MSTest-FSharp/Program.fs
@@ -1,7 +1,1 @@
-open Microsoft.VisualStudio.TestTools.UnitTesting
-
-module MSTestSettings =
-    [<assembly: Parallelize(Scope = ExecutionScope.MethodLevel)>]
-    do()
-
 module Program = let [<EntryPoint>] main _ = 0
